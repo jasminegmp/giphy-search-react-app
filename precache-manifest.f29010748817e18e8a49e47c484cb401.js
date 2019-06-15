c746f6d4f1231df0e9d34b89d1cadef475577e39
jasminegmp <jasminekgump@gmail.com>
2019-06-14 17:29:59 -0700

    Updates

---
+++ precache-manifest.f29010748817e18e8a49e47c484cb401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ebd06e84b1c884fd8b9dad29154bc5",
+    "url": "/giphy-search-react-app/index.html"
+  },
+  {
+    "revision": "81ddcc447b791fe3cfdd",
+    "url": "/giphy-search-react-app/static/css/main.53fbd9b0.chunk.css"
+  },
+  {
+    "revision": "810f1555cc28ec48f08a",
+    "url": "/giphy-search-react-app/static/js/2.badd22ab.chunk.js"
+  },
+  {
+    "revision": "81ddcc447b791fe3cfdd",
+    "url": "/giphy-search-react-app/static/js/main.0ef08380.chunk.js"
+  },
+  {
+    "revision": "728b3affae334aa68649",
+    "url": "/giphy-search-react-app/static/js/runtime~main.a3e2f082.js"
+  }
+]);
